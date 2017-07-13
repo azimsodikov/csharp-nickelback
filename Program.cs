318ceabfec8077b945490000e9702b4d1c73aa9b
Azim <azimsadikov@gmail.com>
2017-07-12 20:07:45 -0500

    "nickelback is gone"

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+            HashSet<(string artist, string song)> allSongs = new HashSet<(string artist, string song)>();
+            allSongs.Add(("Nickelback", "How You Remind Me"));
+            allSongs.Add(("Drake", "Hello"));
+            allSongs.Add(("50 cent", "By By"));
+            allSongs.Add(("Pac", "Hit them up"));
+            allSongs.Add(("Britney", "Airplane"));
+            allSongs.Add(("Rock", "How You Remind Me"));
+            allSongs.Add(("Nickelback", "How You Forget Me"));
+
+            foreach (var item in allSongs)
+            {
+                if (item.Item1 != "Nickelback")
+                {
+                    goodSongs.Add(item);
+                }
+            }
+            foreach (var item in goodSongs)
+            {
+                Console.WriteLine($"{item.Item2} by {item.Item1}");
+            }
+        }
+    }
+}
